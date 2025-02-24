2530e44bdbfb432d629ba02ac90ba2d802e06410
MKmac420 <65330895+MKmac420@users.noreply.github.com>
2025-02-24 11:22:56 +0800

    Update CarRental.java

--- CarRental.java
+++ CarRental.java
@@ -1,48 +1,76 @@
 package pcse002_assignment_solo;
 import javax.swing.JOptionPane;
-//https://docs.oracle.com/en/java/javase/17/docs/api/java.desktop/javax/swing/JOptionPane.html
 
-
-public class ExamEligibility {
+public class CarRental {
 	public static void main(String[] args) {
-		double classNum = 0, classesAttended = 1;
+		double car = 0, rental = 0, priceDaily, priceWeekly, priceMonthly;
+		String carName = "Null";
 		
-		String classSum = JOptionPane.showInputDialog(null,
-	             "How many classes are there in total?", "Class total input.",
-	             JOptionPane.QUESTION_MESSAGE);
-		classNum = Integer.parseInt(classSum);
-		
-		String classAttendance = JOptionPane.showInputDialog(null,
-	             "How many classes did you attend?", "Class attendance input.",
-	             JOptionPane.QUESTION_MESSAGE);
-		classesAttended = Integer.parseInt(classAttendance);
-		if (classNum < classesAttended) {
-			JOptionPane.showMessageDialog(null, "Error, number of classes attended cannot be higher than number of classes total.");
-			return;
+		int carChoice = 0;
+			String carType = JOptionPane.showInputDialog(null,
+			             "Choose a car type.\n1. Compact, $30.50 per day\n2. Sedan, $50.00 per day\n3. SUV, $80.96 per day\nPlease input the number only.", "Car type selection",
+			             JOptionPane.QUESTION_MESSAGE);
+			carChoice = Integer.parseInt(carType);
+			if (carChoice <= 0 || carChoice >= 4) {
+				JOptionPane.showMessageDialog(null, "Error, invalid car type.");
+				return;
 			}
-
-		String assignment1 = JOptionPane.showInputDialog(null,
-	             "Score for assignment 1? (out of 100%)", "Assignment 1 score score input.",
-	             JOptionPane.QUESTION_MESSAGE);
-		int asmt1 = Integer.parseInt(assignment1);
-		
-		String assignment2 = JOptionPane.showInputDialog(null,
-	             "Score for assignment 2? (out of 100%)", "Assignment 2 score input.",
-	             JOptionPane.QUESTION_MESSAGE);
-		int asmt2 = Integer.parseInt(assignment2);
-		
-		String assignment3 = JOptionPane.showInputDialog(null,
-	             "Score for assignment 3? (out of 100%)", "Assignment 3 score input.",
-	             JOptionPane.QUESTION_MESSAGE);
-		int asmt3 = Integer.parseInt(assignment3);
 		
 		
-		int assignmentTotal = ((asmt1 + asmt2 + asmt3) / 3);
-		int attendanceTotal = (int)((classesAttended / classNum) * 100); // using type-casting so there are no decimals
-				
+		int rentalChoice = 0;
+			String rentalLength = JOptionPane.showInputDialog(null,
+			             "Choose rental duration.\n1. Daily, no discount\n2. Weekly, 15% discount\n3. Monthly, 30% discount", "rental duration selection.",
+			             JOptionPane.QUESTION_MESSAGE);
+			rentalChoice = Integer.parseInt(rentalLength);
+			if (rentalChoice <= 0 || rentalChoice >= 4) {
+				JOptionPane.showMessageDialog(null, "Error, invalid duration length.");
+				return;
+			}
+			
+		switch (carChoice) {
+		case 1:
+		car = 30.50;
+		carName = "Compact";
+		break;
+		case 2:
+		car = 50.00;
+		carName = "Sedan";
+		break;
+		case 3:
+		car = 80.96;
+		carName = "SUV";
+		break;
+		}
 		
-		if (assignmentTotal < 50.0 || attendanceTotal < 75.0)
-			JOptionPane.showMessageDialog(null, "Unfortunately, you are not eligible for the exam.\nAttendance: " + attendanceTotal + "%\nAssignment total: " + assignmentTotal + "%\nMinimum attendance: 75%\nMinimum assignment score: 50%");
-		else
-			JOptionPane.showMessageDialog(null, "Congrats, you are eligible for the exam.\nAttendance: " + attendanceTotal + "%\nAssignment total: " + assignmentTotal + "%\nMinimum attendance: 75%\nMinimum assignment total: 50%");	
-	}}
+		switch (rentalChoice) {
+		case 1:
+		rental = 1.0;
+		break;
+		case 2:
+		rental = 0.85;
+		break;
+		case 3:
+		rental = 0.70;
+		break;}
+		
+		priceDaily = Math.round(car * rental * 100.0) / 100.0;
+		int display = (int)(rental * 100); // "switch" does not support double variables
+		
+		switch (display) {
+		case 100:  // if the user chose daily
+				JOptionPane.showMessageDialog(null, "Price for " + carName + " type car is $" + priceDaily + " per day.");
+		break;
+		
+		case 85: // if the user chose weekly
+				priceWeekly = Math.round(car * rental * 7 * 100.0) / 100.0;
+				JOptionPane.showMessageDialog(null, "Price for " + carName + " type car is $" + priceDaily + " per day or $" + priceWeekly + " per week.");
+		break;
+		
+		case 70: // if the user chose monthly
+				priceMonthly = Math.round(car * rental * 30 * 100.0) / 100.0;
+				JOptionPane.showMessageDialog(null, "Price for " + carName + " type car is $" + priceDaily + " per day or $" + priceMonthly + " per month.");
+		break;
+		
+		}
+	}
+}
